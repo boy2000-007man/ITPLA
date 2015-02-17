acbd9cc6db273ca8c2bf9845c350397766ab900c
zero <boy2000_007man@163.com>
2015-02-17 20:13:30 +0800

    1.进行了归一化 2.调整了引力计算式 3.增加了部分自适应 4.全局同时移动 5.自动加减点数 6.修正部分bug 7.更改部分变量名

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -4,7 +4,7 @@
 using namespace sdk;
 Points tmp;
 Points res;
-double edge_length = 80;
+double edge_length = 100;
 MainWidget::MainWidget(QWidget *parent) :
     QWidget(parent)
 {
@@ -15,8 +15,10 @@ MainWidget::MainWidget(QWidget *parent) :
 void MainWidget::paintEvent(QPaintEvent *) {
     QPainter painter(this);
 
-    painter.translate(10, 10);
-    painter.scale(1,1);
+    //painter.translate(10, 10);
+    //painter.scale(1,1);
+    //painter.setWindow(0, 0, 2, 2);
+    //painter.setViewport(0, 0, this->width(), this->height());
 
     painter.setPen(Qt::black);
     for (int i = 0; i < tmp.size(); i++) {
@@ -28,6 +30,16 @@ void MainWidget::paintEvent(QPaintEvent *) {
     for (int i = 0; i < res.size(); i++)
         painter.drawEllipse(res[i].x - edge_length, res[i].y - edge_length, 2 * edge_length, 2 * edge_length);
     painter.setPen(Qt::red);
+    for (int i = 0; i < res.size(); i++) {
+        Point &p1 = res[i];
+        for (int j = 0; j < i; j++) {
+            Point &p2 = res[j];
+            double r_distance = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)) / edge_length;
+            if (0.9 < r_distance && r_distance < 1.1)
+                painter.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
+    }
+    painter.setPen(Qt::blue);
     for (int i = 0; i < res.size(); i++)
         painter.drawRect(res[i].x - 1, res[i].y - 1, 2, 2);
 }
--- sdk.h
+++ sdk.h
@@ -8,13 +8,46 @@
 #define Point pair<double, double>
 #define Points vector<Point >
 #define Vector Point
+#define Vectors Points
 #define x first
 #define y second
-#define ZERO 1e-6
+#define ZERO 1e-9
+#define show_time() printf("[%.3lf]", (double)clock() / CLK_TCK)
 
 namespace sdk {
 using namespace std;
-bool inPolygon(const Points &polygon, const Point &p) {
+
+Points test() {
+    Points polygon;
+    polygon.push_back(make_pair(0, 0));
+    polygon.push_back(make_pair(500, 0));
+    /*polygon.push_back(make_pair(250, 0));
+    polygon.push_back(make_pair(200, 300));
+    polygon.push_back(make_pair(500, 250));
+    */
+    //polygon.push_back(make_pair(500, 500));
+    //polygon.push_back(make_pair(100, 100));
+    polygon.push_back(make_pair(0, 500));
+    return polygon;
+}
+
+Point rand_point(const Point &plb, const Point &prt) {
+    assert(plb.x <= prt.x && plb.y <= prt.y);
+    return make_pair((prt.x - plb.x) * rand() / RAND_MAX + plb.x, (prt.y - plb.y) * rand() / RAND_MAX + plb.y);
+}
+
+Point normalize_point(const Point &p, const double edge_length) {
+    return make_pair(p.x / edge_length, p.y / edge_length);
+}
+
+Points normalize_polygon(const Points &polygon, const double edge_length) {
+    Points normalized_polygon;
+    for (int i = 0; i < polygon.size(); i++)
+        normalized_polygon.push_back(normalize_point(polygon[i], edge_length));
+    return normalized_polygon;
+}
+
+bool in_polygon(const Points &polygon/*normalized_polygon*/, const Point &p/*normalized_point*/) {
     bool result = false;
     for (int i = 0; i < polygon.size(); i++) {
         //printf("result = %d, check edge[%d] start...\n", result, i);
@@ -27,7 +60,7 @@ bool inPolygon(const Points &polygon, const Point &p) {
             if (abs(p1.x + (p.y - p1.y) * (p1.x - p2.x) / (p1.y - p2.y) - p.x) < ZERO)
                 return true;
             else
-            result = !result;
+                result = !result;
         /*printf("p = (%lf, %lf), p1 = (%lf, %lf), p2 = (%lf, %lf)\n", p.x,p.y,p1.x,p1.y,p2.x,p2.y);
         printf("%lf\n", (p1.y - p.y) * (p2.y - p.y));
         printf("%lf\n", abs(p1.y - p2.y));
@@ -42,16 +75,18 @@ bool inPolygon(const Points &polygon, const Point &p) {
     return result;
 }
 
-Point moveInPolygon(const Points &polygon, const Point &p, const Vector &v) {
+Point move_in_polygon(const Points &polygon/*normalized_polygon*/, const Point &p, const Vector &v) {
+    const double v_length = sqrt(pow(v.x, 2) + pow(v.y, 2));
+    if (v_length < ZERO)
+        return p;
     //printf("P(%lf, %lf) V(%lf, %lf)\n", p.x, p.y, v.x, v.y);
-    if (!inPolygon(polygon, p)) {
-        printf("(%.24lf, %.24lf) not in polygon.", p.x, p.y);
+    if (!in_polygon(polygon, p)) {
+        printf("(%.16lf, %.16lf) not in polygon.\n", p.x, p.y);
         exit(0);
     } //else
         //printf("(%.24lf, %.24lf) in polygon.", p.x, p.y);
-    assert(inPolygon(polygon, p));
+    assert(in_polygon(polygon, p));
     double t = 1;
-    //const double vrl = sqrt(pow(v.x, 2) + pow(v.y, 2)) / edge_length;// v relative length
     Vector vn = make_pair(0, 0);
     Point pn = make_pair(p.x + v.x, p.y + v.y);
     for (int i = 0; i < polygon.size(); i++) {
@@ -62,7 +97,9 @@ Point moveInPolygon(const Points &polygon, const Point &p, const Vector &v) {
                en = make_pair(pn.x - p1.x, pn.y - p1.y);
         double n = (e12.x * e.y - e12.y * e.x) * (e12.x * en.y - e12.y * en.x);
         if (abs(v.x * e12.y - v.y * e12.x) < ZERO) {
+            //printf("case 1\n");
         } else if (n < 0) {
+            //printf("case 2\n");
             double t_ = ((p.y - p1.y) * e12.x - (p.x - p1.x) * e12.y) / (v.x * e12.y - v.y * e12.x);
             assert(0 <= t_);
             if (t_ < t) {
@@ -71,124 +108,155 @@ Point moveInPolygon(const Points &polygon, const Point &p, const Vector &v) {
                 vn = make_pair(vnl * e12.x, vnl * e12.y);
             }
         } else if (n < ZERO) {
-            double v_length = sqrt(pow(v.x, 2) + pow(v.y, 2));
-            Point pt = make_pair(p.x + v.x / v_length * ZERO, p.y + v.y / v_length * ZERO);
-            Vector e12 = make_pair(p2.x - p1.x, p2.y - p1.y),
-                   e = make_pair(p.x - p1.x, p.y - p1.y);
-            if (0 < e12.x * e.y - e12.y * e.x)
+            //printf("case 3\n");
+            Vector e12 = make_pair(p2.x - p1.x, p2.y - p1.y);
+            //printf("e12:(%lf, %lf), e:(%lf, %lf)\n", e12.x, e12.y, e.x, e.y);
+            if (0 < e12.x * v.y - e12.y * v.x)
                 continue;
             t = 0;
             double vnl = (e12.x * v.x + e12.y * v.y) * (1 - t) / (pow(e12.x, 2) + pow(e12.y, 2));
             vn = make_pair(vnl * e12.x, vnl * e12.y);
         }
     }
-    if (t < ZERO)
-        t = 0;
-    else
+    if (ZERO < abs(t - 1))
         t *= (1 - ZERO);
     pn = make_pair(p.x + v.x * t, p.y + v.y * t);
     if (sqrt(pow(vn.x, 2) + pow(vn.y, 2)) < ZERO)
         return pn;
     else
-        return moveInPolygon(polygon, pn, vn);
-}
-
-Points test() {
-    Points polygon;
-    polygon.push_back(make_pair(0, 0));
-    polygon.push_back(make_pair(500, 0));
-    /*polygon.push_back(make_pair(250, 0));
-    polygon.push_back(make_pair(200, 300));
-    polygon.push_back(make_pair(500, 250));
-    */
-    polygon.push_back(make_pair(500, 500));
-    //polygon.push_back(make_pair(100, 100));
-    polygon.push_back(make_pair(0, 500));
-    return polygon;
-}
-
-Point randPoint(const Point &p1, const Point &p2) {
-    assert(p1.x < p2.x && p1.y < p2.y);
-    return make_pair((p2.x - p1.x) * rand() / RAND_MAX + p1.x, (p2.y - p1.y) * rand() / RAND_MAX + p1.y);
+        return move_in_polygon(polygon, pn, vn);
 }
 
 Points place(const Points &polygon, double edge_length) {
     assert(2 < polygon.size());
+    const Points normalized_polygon = normalize_polygon(polygon, edge_length);
+
     printf("start place\n");
-    double x_min = polygon[0].x, x_max = polygon[0].x,
-           y_min = polygon[0].y, y_max = polygon[0].y;
-    for (int i = 1; i < polygon.size(); i++) {
-        const Point &p = polygon[i];
-        x_min = min(x_min, p.x);
-        x_max = max(x_max, p.x);
-        y_min = min(y_min, p.y);
-        y_max = max(y_max, p.y);
+    Point plb = normalized_polygon[0],
+          prt = normalized_polygon[0];
+    for (int i = 1; i < normalized_polygon.size(); i++) {
+        const Point &p = normalized_polygon[i];
+        plb.x = min(plb.x, p.x);
+        plb.y = min(plb.y, p.y);
+        prt.x = max(prt.x, p.x);
+        prt.y = max(prt.y, p.y);
     }
+    printf("(%lf, %lf)<->(%lf, %lf)\n", plb.x, plb.y, prt.x, prt.y);
 
     //srand(time(NULL));
     int total = 0xfff,
         inside = 0;
     for (int i = 0; i < total; i++)
-        inside += inPolygon(polygon, randPoint(make_pair(x_min, y_min), make_pair(x_max, y_max)));
-    double area = (x_max - x_min) * (y_max - y_min) * inside / total;
+        inside += in_polygon(normalized_polygon, rand_point(plb, prt));
+    double area = (prt.x - plb.x) * (prt.y - plb.x) * inside / total;
     printf("approximate area = %.6lf\n", area);
 
-    int point_number = int(0.8 * area / (sqrt(3) / 4 * pow(edge_length, 2)));
+    int point_number = int(0.8 * area / (sqrt(3) / 4));
     Points point_set;
     for (int i = 0; i < point_number; i++) {
         Point p;
-        while (!inPolygon(polygon, p = randPoint(make_pair(x_min, y_min), make_pair(x_max, y_max))));
+        while (!in_polygon(normalized_polygon, p = rand_point(plb, prt)));
         point_set.push_back(p);
     }
+
+    show_time();
     printf("start evolove\n");
     double tmp = 0.1;
-        double max_f = 0, max_num = 0;
-    for (double max_distance = edge_length; tmp < max_distance / edge_length; (point_set[max_num] = point_set.back()),point_set.pop_back()) {
-        max_f = 0;
-        max_num = 0;
-    for (int n = 0; n < 500; n++)
-        for (int i = 0; i < point_set.size(); i++) {
-            Point &p1 = point_set[i];
-            Vector f = make_pair(0, 0);
-            for (int j = 0; j < point_set.size(); j++) {
-                Point &p2 = point_set[j];
-                if (i != j) {
+    for (;;) {
+        int number = 0;
+        for (int n = 0; n < 500; n++) {
+            Vectors f(point_set.size(), make_pair(0, 0));
+            double max_repulsion_force = 0;
+            for (int i = 0; i < point_set.size(); i++) {
+                const Point &p1 = point_set[i];
+                Vector &f1 = f[i];
+                for (int j = 0; j < i; j++) {
+                    const Point &p2 = point_set[j];
+                    Vector &f2 = f[j];
+
                     Vector v = make_pair(p1.x - p2.x, p1.y - p2.y);
                     double v_length = sqrt(pow(v.x, 2) + pow(v.y, 2)),
-                           k = pow(edge_length / v_length, 3);
-                    if (v_length <= edge_length)
-                        f = make_pair(f.x + k * v.x, f.y + k * v.y);
-                    else if (ZERO < abs(v_length - edge_length))
-                        f = make_pair(f.x - k * v.x / 8, f.y - k * v.y / 8);
+                           k = pow(v_length, -3) - 1 / v_length;
+                    /*if (v_length <= 1) {
+                        f1 = make_pair(f1.x + k * v.x, f1.y + k * v.y);
+                        f2 = make_pair(f2.x - k * v.x, f2.y - k * v.y);
+                        if (max_repulsion_force < k) {
+                            max_repulsion_force = k;
+                            number = i;
+                        }
+                    } else {
+                        f1 = make_pair(f1.x - k * v.x / 8, f1.y - k * v.y / 8);
+                        f2 = make_pair(f2.x + k * v.x / 8, f2.y + k * v.y / 8);
+                    }*/
+                    if (max_repulsion_force < k) {
+                        max_repulsion_force = k;
+                        number = i;
+                    }
+                    if (k < 0)
+                        k *= 1.0 / (n + 1);
+                    f1 = make_pair(f1.x + k * v.x, f1.y + k * v.y);
+                    f2 = make_pair(f2.x - k * v.x, f2.y - k * v.y);
                 }
             }
-            double f_length = sqrt(pow(f.x, 2) + pow(f.y, 2)),
-                   k = min(0.01 * edge_length / f_length, 1.0 / 27);
-            if (max_f < f_length) {
-                max_f = f_length;
-                max_num = i;
+            double max_f_length = 0;
+            for (int i = 0; i < f.size(); i++) {
+                double f_length = sqrt(pow(f[i].x, 2) + pow(f[i].y, 2));
+                max_f_length = max(max_f_length, f_length);
             }
-            f = make_pair(f.x * k, f.y * k);
+            if (max_f_length < 0.001)
+                break;
+                //return normalize_polygon(point_set, 1 / edge_length);
+            //double k = 0.01 * min(1.0, 1 / max_f_length);
+            double k = 0.01 < max_f_length ? 0.01 / max_f_length : 1;
+            for (int i = 0; i < f.size(); i++) {
+                Point &p = point_set[i];
+                Vector &v = f[i];
+                p = move_in_polygon(normalized_polygon, p, make_pair(k * v.x, k * v.y));
+            }
             //printf("Round: %d, point[%d](%lf, %lf) move(%lf, %lf)", n, i, p1.x, p1.y, f.x, f.y);
-            p1 = moveInPolygon(polygon, p1, f);
             /*double limit = 0.005 * edge_length;
             Point plb = make_pair(-limit, -limit),
                   prt = make_pair(limit, limit);
-            p1 = moveInPolygon(polygon, p1, randPoint(plb, prt));*/
+            p1 = moveInnormalized_Polygon(normalized_polygon, p1, randPoint(plb, prt));*/
             //printf(" now at(%.6lf, %.6lf)\n", p1.x, p1.y);
         }
-        max_distance = 0;
+        double max_distance = 0;
         for (int i = 0; i < point_set.size(); i++) {
             Point &p1 = point_set[i];
             for (int j = 0; j < i; j++) {
                 Point &p2 = point_set[j];
-                max_distance = max(max_distance, max(0.0, edge_length - sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2))));
+                max_distance = max(max_distance, max(0.0, 1 - sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2))));
             }
         }
+        if (max_distance < tmp) {
+            //break;
+            bool go_on = false;
+            for (int i = 0; i < normalized_polygon.size(); i++) {
+                const Point &p1 = normalized_polygon[i];
+                bool k = true;
+                for (int j = 0; j < point_set.size() && k; j++) {
+                    Point &p2 = point_set[j];
+                    k = (1 <= sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)));
+                }
+                go_on |= k;
+                if (k) {
+                    printf("add point[%d](%lf, %lf)\n", point_set.size(), p1.x, p1.y);
+                    point_set.push_back(p1);
+                }
+            }
+            if (!go_on)
+                break;
+        } else {
+            printf("delete point[%d](%lf, %lf)\n", number, point_set[number].x, point_set[number].y);
+            point_set[number] = point_set.back();
+            point_set.pop_back();
+        }
     }
+    show_time();
     printf("end evolove\n");
-    return point_set;
+    return normalize_polygon(point_set, 1 / edge_length);
 }
+
 }
 
 #endif // SDK_H
